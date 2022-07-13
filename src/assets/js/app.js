d4fa8e7b92a3af045d85291e06027249b5236404
Vladimir <htc.zvv@gmail.com>
2022-07-13 10:31:22 +0300

    added modal window

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,9 @@
 /* Bootstrap JS */
+//= ../../../node_modules/bootstrap/js/dist/dom/data.js
+//= ../../../node_modules/bootstrap/js/dist/dom/event-handler.js
+//= ../../../node_modules/bootstrap/js/dist/dom/manipulator.js
+//= ../../../node_modules/bootstrap/js/dist/dom/selector-engine.js
+//= ../../../node_modules/bootstrap/js/dist/base-component.js
 //= ../../../node_modules/bootstrap/js/dist/modal.js
 
 /* Slick-carousel JS */
